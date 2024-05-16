f6947a7ac0244ed32da319b5b8b1a10d06b1ae78
Fabio Jose Cintra <fjcintra90@gmail.com>
2024-05-16 19:43:27 -0300

    Separated html into head (with html header) and home (main page)

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
